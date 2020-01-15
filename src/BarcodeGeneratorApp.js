c7339eb469ce5b7f74b1213dd85a480a600e3bb7
U-YE-AW15R3\Ye <wangyeee@gmail.com>
2020-01-15 21:04:52 +1300

    Create new app

---
+++ src/BarcodeGeneratorApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class BarcodeGeneratorApp extends React.Component {
+    render() {
+        return (<div>BarcodeGeneratorApp</div>);
+    }
+}
+
+export default BarcodeGeneratorApp;
